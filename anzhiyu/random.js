5fe8c448382d472a27927008d0d49d422deab6e2
Lucas-Fang <1816072827@qq.com>
2025-09-10 18:30:59 +0800

    Site updated: 2025-09-10 18:30:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/10/hello-world/"];function toRandomPost(){
+var posts=["1970/01/01/test-blog/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
